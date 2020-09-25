a26d5bb780d16f7db03ad32170cfa124ea472a6d
cosmonet-fr <stephane.billois@hotmail.com>
2020-09-25 10:08:28 +0200

    fade effect

--- src/components/AllProjects.vue
+++ src/components/AllProjects.vue
@@ -2,59 +2,75 @@
   <div class="allProjects">
 
     <div class="oneProject orinoco" @mouseover="orinoco = true" @mouseleave="orinoco = false">
-      <div class="text" v-show="orinoco">
-        <h2>Orinoco</h2>
-        <p>Réalisation d'un site de e-commerce.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="orinoco">
+          <h2>Orinoco</h2>
+          <p>Réalisation d'un site de e-commerce.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject groupomania" @mouseover="groupomania = true" @mouseleave="groupomania = false">
-      <div class="text" v-show="groupomania">
-        <h2>Groupomania</h2>
-        <p>Réalisation d'un réseau social d'entreprise.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="groupomania">
+          <h2>Groupomania</h2>
+          <p>Réalisation d'un réseau social d'entreprise.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject sauce" @mouseover="sauce = true" @mouseleave="sauce = false">
-      <div class="text" v-show="sauce">
-        <h2>Piquante</h2>
-        <p>Réalisation d'une API de notation de sauces.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="sauce">
+          <h2>Piquante</h2>
+          <p>Réalisation d'une API de notation de sauces.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject ohmyfood" @mouseover="ohmyfood = true" @mouseleave="ohmyfood = false">
-      <div class="text" v-show="ohmyfood">
-        <h2>ohmyfood</h2>
-        <p>Réalisation d'un site référençant les menus de restaurants.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="ohmyfood">
+          <h2>ohmyfood</h2>
+          <p>Réalisation d'un site référençant les menus de restaurants.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject babm" @mouseover="babm = true" @mouseleave="babm = false">
-      <div class="text" v-show="babm">
-        <h2>Belles Autos, belles motos</h2>
-        <p>Réalisation d'un site de présentation d'un événement.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="babm">
+          <h2>Belles Autos, belles motos</h2>
+          <p>Réalisation d'un site de présentation d'un événement.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject chouette" @mouseover="chouette = true" @mouseleave="chouette = false">
-      <div class="text" v-show="chouette">
-        <h2>La chouette agence</h2>
-        <p>Obtimisation de l'accessibilité et du SEO pour le site d'une agence de communication.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="chouette">
+          <h2>La chouette agence</h2>
+          <p>Obtimisation de l'accessibilité et du SEO pour le site d'une agence de communication.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject cv" @mouseover="cv = true" @mouseleave="cv = false">
-      <div class="text" v-show="cv">
-        <h2>Curriculum vitae</h2>
-        <p>Réalisation de mon CV en ligne.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="cv">
+          <h2>Curriculum vitae</h2>
+          <p>Réalisation de mon CV en ligne.</p>
+        </div>
+      </transition>
     </div>
 
     <div class="oneProject michelin" @mouseover="michelin = true" @mouseleave="michelin = false">
-      <div class="text" v-show="michelin">
-        <h2>Michelin</h2>
-        <p>Réalisation d'un prototype d'une web app pour un musée.</p>
-      </div>
+      <transition name="slide-fade">
+        <div class="text" v-show="michelin">
+          <h2>Michelin</h2>
+          <p>Réalisation d'un prototype d'une web app pour un musée.</p>
+        </div>
+      </transition>
     </div>
 
 
@@ -73,7 +89,8 @@
         babm: false,
         chouette: false,
         cv: false,
-        michelin: false
+        michelin: false,
+        test: true
       }
     },
     name: 'AllProjects',
@@ -96,7 +113,7 @@
     background-repeat: no-repeat;
     background-size: cover;
   }
-  .orinoco { background-image: url(../assets/orinoco.jpg); }
+  .orinoco { background-image: url(../assets/orinoco.jpg);}
   .groupomania { background-image: url(../assets/groupomania.jpg); }
   .sauce { background-image: url(../assets/piquante.jpg); }
   .ohmyfood { background-image: url(../assets/ohmyfood.jpg); }
@@ -123,4 +140,16 @@
       //margin-bottom: 5em;
     }
   }
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+  /* .slide-fade-leave-active below version 2.1.8 */ {
+  //transform: translateX(-10em);
+  opacity: 0;
+}
+
 </style>
